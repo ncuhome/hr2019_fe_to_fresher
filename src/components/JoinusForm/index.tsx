65abc6b34cd50b65bf1c62eb3373a9c6680c081b
southrock <southrock@foxmail.com>
2019-07-23 17:51:20 +0800

    :construction: 报名页面基本完成

---
+++ src/components/JoinusForm/index.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import './style.css';
+
+export interface formType {
+  value:{
+    name:string
+    gender:string,
+    groupType:string,
+    email:string,
+    phonenumber:number,
+    classname:string,
+    introduce:string
+  },
+  onChange: any
+}
+
+function handleSubmit(event:any) {
+  event.persist();
+  event.preventDefault();
+  console.log(event);
+}
+
+export default function JoinusForm(props:formType) {
+
+  const { name,gender,groupType,email,phonenumber, classname,introduce} = props.value;
+  const handleChange = props.onChange;
+
+  const isMale = (gender === '男') ? true : false;
+
+  return (
+    <form action="" className="form" onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="name">姓名</label>
+        <input type="text" name="name" id="name" value={name} onChange={handleChange} />
+      </p>
+      <p>
+        <label htmlFor="gender">性别</label>
+        <div className="radio-container">
+          <label htmlFor="male">男</label>
+          <input type="radio" name="gender" id="male" value="男" defaultChecked onClick={handleChange} />
+          <span style={{visibility: (!isMale ? "hidden": "visible")}} />
+        </div>
+        <div className="radio-container">
+          <label htmlFor="female">女</label>
+          <input type="radio" name="gender" id="female" value="女" onClick={handleChange} />
+          <span style={{visibility: (isMale ? "hidden": "visible")}} />
+        </div>
+      </p>
+      <p>
+        <label htmlFor="groupType">组别</label>
+        <select name="groupType" id="groupType" value={groupType} onChange={handleChange}>
+          <option value="设计">设计组</option>
+          <option value="研发">研发组</option>
+          <option value="运营">运营组</option>
+          <option value="产品">产品组</option>
+          <option value="行政">行政组</option>
+        </select>
+      </p>
+      <p>
+        <label htmlFor="email">邮箱</label>
+        <input type="email" name="email" id="email" value={email} onChange={handleChange} />
+      </p>
+      <p>
+        <label htmlFor="phonenumber">手机号码</label>
+        <input type="tel" name="phonenumber" id="phonenumber" value={phonenumber} onChange={handleChange} />
+      </p>
+      <p>
+        <label htmlFor="class">专业班级</label>
+        <input type="text" name="classname" id="classname" value={classname} onChange={handleChange} />
+      </p>
+      <p>
+        <label htmlFor="introduce">自我介绍</label>
+      </p>
+      <textarea 
+        name="introduce"
+        id="introduce"
+        placeholder="可以说说自己的爱好，特长和有趣的经历哦~ PS:还可以说说选择该组的理由呢！"
+        value={introduce}
+        onChange={handleChange}
+      />
+      <div className="btn-container">
+        <input type="submit" value="提交"/>
+      </div>
+    </form>
+  )
+}
--- src/pages/application/index.tsx
+++ src/pages/application/index.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react';
 import anime from 'animejs';
+import JoinusForm from '../../components/JoinusForm/index';
 import './style.css';
 
 const { useState, useEffect } = React;
 
+
 const circle_arrow = require('../../assets/svg/circle_arrow.svg');
 
+
 export default function Application(props: any) {
-  const [formValues, setFormValues] = useState(null);
+  const [formValues, setFormValues] = useState({
+    name: "",
+    gender: "男",
+    groupType: "",
+    email: "",
+    phonenumber: undefined,
+    classname: "",
+    introduce:""
+  });
   const [stageIndex, setStageIndex] = useState(0);
   const [ncuhomeStyle, setNcuhomeStyle] = useState({});
+
+  const handleChange = (event:any):void => {
+    const { name, value } = event.target;
+    const newValue:any = {...formValues};
+    newValue[name] = value;
+    setFormValues(newValue);
+    console.log(name+' '+value);
+    console.log(formValues);
+  }
 
   useEffect(() => {
     // 计算icon位置使表单始终只露出标题
@@ -24,7 +44,7 @@ export default function Application(props: any) {
 
   useEffect(() => {
     anime({
-      targets: '.icon-container',
+      targets: '.icon-container img',
       translateY: '10px',
       loop: true,
       autoplay: true,
@@ -34,8 +54,8 @@ export default function Application(props: any) {
 
   return (
     <div className="container">
-      <div className="planet-container">
-        <div className="planet">
+      <div className="circle-container">
+        <div className="circle">
         </div>
       </div>
       <div className="ncuhome-container">
@@ -52,64 +72,7 @@ export default function Application(props: any) {
           <p className="english-headline">Ncuhome Application Form</p>
           <p className="headline">星球入驻申请单</p>
         </div>
-        <form className="form">
-          <p>
-            <label htmlFor="name">姓名</label>
-            <input type="text" name="name" id="name"/>
-          </p>
-          <p>
-            <label htmlFor="gender">性别</label>
-            <span>
-              <label htmlFor="male">男</label>
-              <input type="radio" name="gender" id="male"/>
-              <label htmlFor="female">女</label>
-              <input type="radio" name="gender" id="female"/>
-            </span>
-          </p>
-          <p>
-            <label htmlFor="sid">学号</label>
-            <input type="text" name="sid" id="sid"/>
-          </p>
-          <p>
-            <label htmlFor="groupType">组别</label>
-            <select name="groupType" id="groupType">
-              <option value="设计">设计组</option>
-              <option value="研发">研发组</option>
-              <option value="运营">运营组</option>
-              <option value="产品">产品组</option>
-              <option value="行政">行政组</option>
-            </select>
-          </p>
-          <p>
-            <label htmlFor="telephone">TEL</label>
-            <input type="tel" name="telephone" id="telephone"/>
-          </p>
-          <p>
-            <label htmlFor="email">邮箱</label>
-            <input type="email" name="email" id="email"/>
-          </p>
-          <p>
-            <select name="socialMedia" id="socialMedia">
-              <option value="QQ">QQ</option>
-            </select>
-            <input type="text" placeholder="请选填自己活跃的社交账号（运营必填）"/>
-          </p>
-          <p>
-            <label htmlFor="introduce">自我介绍</label>
-          </p>
-          <p>
-            <textarea 
-              name="introduce"
-              id="introduce"
-              cols={30}
-              rows={10}
-              placeholder="可以说说自己的爱好，特长和有趣的经历哦~ PS:还可以说说选择该组的理由呢！"
-            />
-          </p>
-          <div className="btn-container">
-            <button type="submit">提交</button>
-          </div>
-        </form>
+        <JoinusForm value={formValues} onChange={handleChange} />
       </div>
     </div>
   )
