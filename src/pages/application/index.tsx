f929ee53a4709134f5740fbce6a50ba8c9b32fc0
southrock <southrock@foxmail.com>
2019-08-02 12:06:14 +0800

    :construction: :poop: 完善报名页面

--- src/pages/application/index.tsx
+++ src/pages/application/index.tsx
@@ -1,24 +1,28 @@
 import * as React from 'react';
 import anime from 'animejs';
+// import axios from 'axios';
+import { withRouter } from 'react-router-dom';
+import BackArrow from '../../components/BackArrow';
 import JoinusForm from '../../components/JoinusForm/index';
 import './style.css';
 
 const { useState, useEffect } = React;
 
+const xiaojiayuan = require('../../assets/png/xiaojiayuan.png');
 const ncuhome = require('../../assets/png/ncuhome_planet.png');
 const arrowUp = require('../../assets/svg/form_arrow_up.svg');
 
 let formRef:any;
 
-export default function Application(props: any) {
+const Application = (props: any) => {
   const [formValues, setFormValues] = useState({
     name: "",
     gender: "男",
-    groupType: "",
+    department: "",
     email: "",
-    phonenumber: "",
-    classname: "",
-    introduce:""
+    phone: "",
+    clazz: "",
+    introduction:""
   });
   const [isUpper, setIsUpper] = useState(true);
   const [ncuhomeStyle, setNcuhomeStyle] = useState({});
@@ -34,21 +38,28 @@ export default function Application(props: any) {
     event.persist();
     event.preventDefault();
     console.log(formValues);
+    // axios.post('http://freshman.guoxy.top/api/freshman/signup',{
+      
+    // });
   }
 
   const handleArrowClick = () => {
     const scrollWindow = anime({
       targets: "html,body",
       duration: 1000,
-      scrollTop: formRef.offsetTop,
+      scrollTop: formRef.offsetTop-10,
       easing: "easeInOutQuad",
       autoplay: true,
     });
   }
 
+  const handleBackClick = (event:any) => {
+    props.history.push("/group");
+  }
+
   useEffect(() => {
     // 计算工作室名称下外边框大小使表单始终只露出标题与姓名栏
-    const heightDif:number = window.innerHeight  - 667
+    const heightDif:number = window.innerHeight  - 667;
     const widthDif:number = window.innerWidth - 375;
     // 以下参数与布局和图片尺寸有关
     const extHeightByHeight: number = heightDif*1.0
@@ -73,6 +84,7 @@ export default function Application(props: any) {
   },[]);
 
   useEffect(() => {
+    // 有冗余，但修改会发现bug，TODO
     const planetAnime = anime({
       targets: ".application-container .ncuhome-planet-container img",
       direction: 'normal',
@@ -123,6 +135,7 @@ export default function Application(props: any) {
 
   return (
     <div className="application-container">
+      <BackArrow onClick={handleBackClick} />
       <div className="ncuhome-planet-container">
         <img src={ncuhome} className="ncuhome-planet" alt="ncuhome" />
       </div>
@@ -142,6 +155,9 @@ export default function Application(props: any) {
         </div>
         <JoinusForm value={formValues} onChange={handleChange} onSubmit={handleSubmit} />
       </div>
+      <img src={xiaojiayuan} alt="小家园" id="xiaojiayuan"/>
     </div>
   )
 }
+
+export default withRouter(Application)
