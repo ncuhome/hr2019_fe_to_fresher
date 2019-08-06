01576086d1f85168ef808623729834bd45f7c7ae
southrock <southrock@foxmail.com>
2019-08-06 16:06:57 +0800

    :construction: 增加谷歌分析检测路由变化

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
+import { History } from 'history';
 import enquire from 'enquire.js';
+import * as ReactGA from 'react-ga';
 import Home from './pages/home';
 import ProductIntro from './pages/productintro';
 import DepartmentsIntro from './pages/departmentintro';
@@ -10,9 +12,33 @@ import config from './config';
 
 
 const { useState, useEffect } = React;
-const { desktopQuery } = config;
+const { desktopQuery, gaTrackingCode } = config;
 
-export default function App() {
+ReactGA.initialize(gaTrackingCode,{ debug:true });
+
+const Container = (props:{history:History,children?:any}) => {
+
+  const { history } = props;
+
+  useEffect(() => {
+    ReactGA.pageview(history.location.pathname);
+    const unlisten = history.listen((location) => {
+      ReactGA.pageview(location.pathname);
+    });
+    return () => {
+      unlisten();
+    }
+  },[]);
+
+  return (
+    <React.Fragment>
+      {props.children}
+    </React.Fragment>
+  )
+}
+const RouteContainer = withRouter(Container);
+
+const App = () => {
 
   const [ isMobile, setIsMobile ] = useState(true);
 
@@ -33,13 +59,17 @@ export default function App() {
 
   return isMobile ? (
     <Router>
-      <Route exact path="/" component={Home} />
-      <Route path="/product" component={ProductIntro} />
-      <Route path="/department" component={DepartmentsIntro} />
-      <Route path="/application" component={Application} />
+      <RouteContainer>
+        <Route exact path="/" component={Home} />
+        <Route path="/product" component={ProductIntro} />
+        <Route path="/department" component={DepartmentsIntro} />
+        <Route path="/application" component={Application} />
+      </RouteContainer>
     </Router>
   ) :
   (
     <div>请使用移动端打开</div>
   )
 };
+
+export default App;
--- src/config.tsx
+++ src/config.tsx
@@ -162,4 +162,5 @@ export default {
       ],
     },
   ],
+  gaTrackingCode: "UA-80324378-13",
 };
