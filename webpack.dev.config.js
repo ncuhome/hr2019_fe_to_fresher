1855c102e473d80c1810314178bb1b648b158abb
southrock <southrock@foxmail.com>
2019-08-11 12:28:12 +0800

    :wrench: 配置webpack代理

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -13,6 +13,12 @@ config.devServer = {
   host: host,
   historyApiFallback: true,
   disableHostCheck: true,
+  proxy: {
+    '/api': {
+      target: 'https://2019hr.ncuos.com',
+      changeOrigin: true,
+    }
+  }
 }
 config.plugins.push(new webpack.HotModuleReplacementPlugin());
 module.exports = config;
