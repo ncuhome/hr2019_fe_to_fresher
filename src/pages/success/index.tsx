730bcff5320fe4285c97752e2e94af5245fd1dbb
“Jungbey” <1952325619@qq.com>
2020-09-08 11:56:25 +0800

    :feat:  优化内容展示方法

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -2,14 +2,48 @@ import * as React from 'react';
 import './style.css'
 import Tweet from './components/Tweet'
 
+interface TweetType {
+  href: string;
+  cover: any;
+  title: string;
+  author_icon: any;
+  author: string;
+}
+
 const landing = require('../../assets/png/success_landing.png')
 const arrow = require('../../assets/svg/success_arrow.svg')
-const cover_1 = require('../../assets/png/success_cover-1.png')
+const cover_work = require('../../assets/png/success_cover-1.png')
 const zhihu_icon = require('../../assets/svg/zhihu-icon.svg')
-const cover_2 = require('../../assets/png/success_cover-2.png')
+const cover_mario = require('../../assets/png/success_cover-2.png')
 const bilibili_icon = require('../../assets/svg/bilibili-icon.svg')
 
+const Tweets: TweetType[] = [
+  {
+    href: 'https://www.zhihu.com/question/61745974',
+    cover: cover_work,
+    title: '在南昌大学家园工作室工作是怎样一种体验？',
+    author_icon: zhihu_icon,
+    author: '来自知乎er的回答'
+  },
+  {
+    href: 'https://www.bilibili.com/video/BV1a441167tw',
+    cover: cover_mario,
+    title: '在南昌大学家园工作室工作是怎样一种体验？',
+    author_icon: bilibili_icon,
+    author: '来自家园工作室的小破站'
+  }
+]
+
 const Success: React.FC = () => {
+  const content = Tweets.map(tweet =>
+    <Tweet
+      href={tweet.href}
+      cover={tweet.cover}
+      title={tweet.title}
+      author_icon={tweet.author_icon}
+      author={tweet.author}
+    />
+  )
   return (
     <div className="success-container" >
       <div className="success-tip" >
@@ -32,20 +66,7 @@ const Success: React.FC = () => {
       </div>
       <div className="msg">
         <h2>宇宙漫游指北</h2>
-        <Tweet
-          href='https://www.zhihu.com/question/61745974'
-          cover={cover_1}
-          title='在南昌大学家园工作室工作是怎样一种体验？'
-          author_icon={zhihu_icon}
-          author='来自知乎er的回答'
-        />
-        <Tweet
-          href='https://www.bilibili.com/video/BV1a441167tw'
-          cover={cover_2}
-          title='在南昌大学家园工作室工作是怎样一种体验？'
-          author_icon={bilibili_icon}
-          author='来自家园工作室的小破站'
-        />
+        {content}
       </div>
     </div>
   )
