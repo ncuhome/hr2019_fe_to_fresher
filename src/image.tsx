bbad804799ddefdc5f6fbe95e0404d62aca2e4aa
FuzzyFade <1339184537@qq.com>
2021-08-28 19:15:05 +0800

    chore: 补齐注释 & 升级依赖

--- src/image.tsx
+++ src/image.tsx
@@ -1,21 +1,25 @@
+/**
+ * 图片资源预加载
+ */
 const loadImage = (cb: Function) => {
-  // 使用上下文
   let count = 0;
+
+  const increase = () => {
+    count++;
+    cb(count, pictureLength);
+  };
+
+  // window.IMAGES_DATA is defined by vite-plugin-image-url
   const pictures: string[] = (window as any).IMAGES_DATA;
   const pictureLength = pictures.length;
+
+  // load images by urls
   pictures.forEach((item) => {
     const pic = new Image();
-    pic.onload = () => {
-      count++;
-      cb(count, pictureLength);
-    };
-    pic.onerror = () => {
-      count++;
-      cb(count, pictureLength);
-    }
+    pic.onload = increase;
+    pic.onerror = increase;
     pic.src = item;
   });
-  return;
 };
 
 export default loadImage;
